e11344493cb7a2b318dd0ea3b096b2d92916604f
shangqigao <shqgao@163.com>
2021-04-18 10:53:04 +0800

    fixed bugs

--- SpBCD/Spminimization.m
+++ SpBCD/Spminimization.m
@@ -16,14 +16,15 @@ g(g>0)=g(g>0)./d(g>0);
 k = length(g(g>0));
 t = (p*(1-p)/2)^(1/(2-p));
 if k == 0
-    tau = tau;
+    tau = tau*0.5;
 elseif k < length(d)
     d1 = (d(k+1)/(t+p*t^(p-1)))^(2-p);
     d2 = (d(k)/(t+p*t^(p-1)))^(2-p);
-    tau = 0.5*d1 + 0.5*d2;
+    tau = 0.9*d1 + 0.1*d2;
 else
     d2 = (d(k)/(t+p*t^(p-1)))^(2-p);
-    tau = d2/10;
+    tau = d2*0.1;
 end
 M=U*diag(g)*U'*X;
+tau
 end
